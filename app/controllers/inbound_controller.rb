32de3922d71856ef4d4e3fdb4e3ddc83def2a25d
Matt Van Veenendaal <m@mattvv.com>
2014-06-21 15:57:45 -0700

    decode html before sending

--- app/controllers/inbound_controller.rb
+++ app/controllers/inbound_controller.rb
@@ -7,20 +7,21 @@ class InboundController < ApplicationController
     #todo: get the list from the To param
     #todo: ensure person who has sent (From) to the list is on the list.
     email = params
-
+    coder = HTMLEntities.new
+    html = coder.decode(email[:HtmlBody])
     message = Mail.new do
       from            'team@dragons.email' #Adjust from to be from the original author.
       to              'Matt Van <mattv@mumsweb.net>' #use bcc to send
       bcc             'Matt Van <mattvanveenendaal@gmail.com>' #bcc
       subject         'Test Email from the Dragons'
-      # text_part do
-      #   body email[:TextBody]
-      # end
+      text_part do
+        body email[:TextBody]
+      end
 
-      # html_part do
+      html_part do
         content_type  'text/html; charset=UTF-8'
-        body email[:HtmlBody]
-      # end
+        body html
+      end
 
       delivery_method Mail::Postmark, :api_key => ENV['POSTMARK_API_KEY']
     end
