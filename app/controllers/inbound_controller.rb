fef3178b4eeb4a3e232b322c91b501377ce641a5
Matt Van Veenendaal <m@mattvv.com>
2014-06-21 15:33:16 -0700

    closures

--- app/controllers/inbound_controller.rb
+++ app/controllers/inbound_controller.rb
@@ -4,16 +4,20 @@ class InboundController < ApplicationController
   def create
     puts 'Inbound Email'
 
+    #todo: get the list from the To param
+    #todo: ensure person who has sent to the list is on the list.
+    email = params
+
     message = Mail.new do
       from            'team@ladragons.com'
       to              'Matt Van <mattv@mumsweb.net>'
       subject         'Test Email from the Dragons'
       text_part do
-        body params[:TextBody]
+        body email[:TextBody]
       end
 
       html_part do
-        body params[:HtmlBody]
+        body email[:HtmlBody]
       end
 
       delivery_method Mail::Postmark, :api_key => ENV['POSTMARK_API_KEY']
