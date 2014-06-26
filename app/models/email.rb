4155621aad375737cfaf465cb8e99728b0cb6ec2
Matt Van Veenendaal <m@mattvv.com>
2014-06-26 16:20:32 -0700

    use block

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,7 @@
 class Email < ActiveRecord::Base
   has_and_belongs_to_many :lists
 
-  default_scope order('name ASC')
+  default_scope { order('name ASC') }
 
   validates :email, uniqueness:  true
 end
