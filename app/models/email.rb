a5d55a7310a37eb5bc00253a921be57f68c6543e
Matt Van Veenendaal <m@mattvv.com>
2014-06-26 16:29:15 -0700

    only allow one person per list

--- app/models/email.rb
+++ app/models/email.rb
@@ -4,4 +4,12 @@ class Email < ActiveRecord::Base
   default_scope { order('name ASC') }
 
   validates :email, uniqueness:  true
+
+  def self.not_in_list(list)
+    if list.emails.count > 0
+      Email.where('id not in (?)', list.emails.all.map(&:id)).all
+    else
+      Email.all
+    end
+  end
 end
