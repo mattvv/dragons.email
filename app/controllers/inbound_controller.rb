44a39c02a2dde77d514cdc5e83dffe9ce6574bdb
Matt Van Veenendaal <m@mattvv.com>
2014-09-18 12:27:12 -0500

    don't base64 decode attachment

--- app/controllers/inbound_controller.rb
+++ app/controllers/inbound_controller.rb
@@ -90,7 +90,7 @@ class InboundController < ApplicationController
 
     unless params[:Attachments].nil?
       params[:Attachments].each do |attachment|
-        message.attachments << { type: attachment[:ContentType], name: attachment[:Name], content: Base64.decode64(attachment[:Content]) }
+        message.attachments << { type: attachment[:ContentType], name: attachment[:Name], content: attachment[:Content] }
       end
     end
 
